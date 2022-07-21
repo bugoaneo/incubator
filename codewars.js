cb59bc3d9718049166c823c41d4944b7a8e2e069
Полина <bugoaneo@users.noreply.github.com>
2022-07-21 18:36:24 +0700

    cdws staff

--- codewars.js
+++ codewars.js
@@ -467,3 +467,29 @@ console.log(lookUpProfile("Sherlock", "likes"));
 console.log(lookUpProfile("Harry", "likes"));
 console.log(lookUpProfile("Bob", "potato"));
 console.log(lookUpProfile("Akira", "address"));
+
+
+
+/*Task*/
+//Coding in function grabDoll.function accept 1 parameter: dolls.it's a string array, a list of some dolls.
+//You need traverse dolls by using for loop.If element is "Hello Kitty" or "Barbie doll", you should push it to a bag(bag is an array, I've defined in the function); if it's other strings, we should use continue skip it.
+//When the bag has three element, bag is full.You should use break jump out the loop; If bag is not full, you should traverse dolls until the last element.
+//Return the bag after for loop finished.
+//You should use for, break and continue in your code.otherwise, your solution may not pass this kata.
+
+function grabDoll(dolls) {
+  var bag = [];
+  for (let i = 0; i < dolls.length; i++) {
+    if (dolls[i] !== "Hello Kitty" && dolls[i] !== "Barbie doll") {
+      continue;
+    } else if (bag.length >= 3) break;
+    bag.push(dolls[i]);
+  }
+
+  return bag;
+}
+
+console.log(grabDoll(["Mickey Mouse", "Hello Kitty", "Snow white"]));//, ["Hello Kitty"]
+console.log(grabDoll(["Mickey Mouse", "Hello Kitty", "Hello Kitty", "Snow white"]));//, ["Hello Kitty", "Hello Kitty"]
+console.log(grabDoll(["Mickey Mouse", "Hello Kitty", "Hello Kitty", "Barbie doll", "Snow white"]));//, ["Hello Kitty", "Hello Kitty", "Barbie doll"]
+console.log(grabDoll(["Mickey Mouse", "Barbie doll", "Hello Kitty", "Hello Kitty", "Hello Kitty", "Snow white"]));//, ["Barbie doll", "Hello Kitty", "Hello Kitty"]
